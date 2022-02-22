bc76bdd120a8a55bb33189cdcea0a4bf63534df3
Rylan Boothman <boothmanrylan@gmail.com>
2022-02-22 12:52:25 -0500

    documentation

--- sequencer/match_sequence.py
+++ sequencer/match_sequence.py
@@ -1,7 +1,26 @@
+"""Methods that find earliest occurences of exact sequences in images."""
+
 import ee
 
 
-def match_sequence(events, sequence, offset):
+def match_sequence(events, sequence, offset=0):
+    """Finds the earliest occurence of sequence in events.
+
+    Determines which, if any, pixels contain sequence in them and at what
+    band index in events the sequence first appears (plus offset to allow
+    the point of interest to occur part way through the sequence).
+
+    Args:
+        events: An ee.Image whose bands represent classifications through time.
+        sequence: An ee.List of class values to search for e.g. [1, 2, 2, 2].
+        offset: An integer representing the offset of from the start of the
+            sequence to the point of interest in the sequence. Default to 0 to
+            return the start of the sequence.
+
+    Returns:
+        An ee.Image with one band whose values are the band index of the input
+        image where sequence first occurs plus offset
+    """
     num_events = events.bandNames().length()
     seq_length = sequence.length()
 
@@ -15,12 +34,32 @@ def match_sequence(events, sequence, offset):
         result = seq_im.eq(compare_to).reduce(ee.Reducer.allNonZero())
         return prev.addBands(result.selfMask())
 
+    # slice(1) to drop the blank image iterate starts with
     checks = ee.Image(indices.iterate(_check, ee.Image())).slice(1)
     matches = checks.multiply(indices_im).add(offset)
     return matches.reduce(ee.Reducer.min())
 
 
 def match_sequences(events, sequences, offsets):
+    """Finds the earliest occurence of any of sequences in events.
+
+    Determines which, if any, pixels in events contain one of sequences in
+    them and at what band index in events the first of sequences appears (plus
+    some offset to allow the point of interest to occur part way through a
+    sequence).
+
+    Args:
+        events: An ee.Image whose bands represent classifications through time.
+        sequences: An ee.List of ee.List of integers representing each of the
+            sequences of class values to search for.
+        offsets: An ee.List of intergers representing the offsets for each of
+            sequences.
+
+    Returns:
+        An ee.Image with one band whose values represent the band index of
+        events where the first of sequences occurs plus some sequence specific
+        offset.
+    """
     indices = ee.List.sequence(0, sequences.length().subtract(1))
 
     def _match(index):
--- sequencer/stable_sequence.py
+++ sequencer/stable_sequence.py
@@ -1,7 +1,24 @@
+"""Methods that find the earliest occurence of a stable sequence in an image.
+
+A stable sequence of a given value is at least X consecutive observations of
+the value with at most N masked pixels and at most M errors occuring before the
+Xth observation of the value of interest.
+"""
+
 import ee
 
 
 def accumulative_sum(image):
+    """Performs an accumulative sum on image treating masked pixels as 0s.
+
+    Args:
+        image: An ee.Image
+
+    Returns:
+        An ee.Image with the same number of bands as the input image where
+        pixels values in band N represent the sum of all values in that pixel
+        in the first N bands.
+    """
     bands = image.bandNames()
 
     def _sum(index, prev):
@@ -11,10 +28,32 @@ def accumulative_sum(image):
         return prev.add(new_image)
 
     sums = bands.iterate(_sum, ee.List([ee.Image(0)]))
+
+    # slice(1) to drop the blank image iterate starts with
     return ee.ImageCollection.fromImages(sums).toBands().slice(1)
 
 
 def find_stable_sequence(events, value, min_stability, max_masked, max_errors):
+    """Finds the first stable sequence of value in events.
+
+    Args:
+        events: An ee.Image whose bands represent classifications through time.
+        value: An integer to search for a stable sequence of.
+        min_stability: An integer representing the minimum number of
+            consecutive observations of value that must occur for a sequence
+            to be considered stable.
+        max_masked: An integer representing the maximum number of masked
+            pixels that can occur in a sequence before min_stability
+            observations and allow the sequence to still be considered stable.
+        max_errors: An integer representing the maximum number of errors (i.e.
+            pixels that do not equal value) that can occur in a sequence before
+            min_stability observations and allwo the sequence to still be
+            considered stable.
+
+    Returns:
+        An ee.Image with band whose values represent the band index in the
+        input image where the earliest stable sequence of values occurs.
+    """
     seq_length = ee.Number(max_masked).add(max_errors).add(min_stability)
     num_events = events.bandNames().length()
     indices = ee.List.sequence(0, num_events.subtract(1))
@@ -28,11 +67,19 @@ def find_stable_sequence(events, value, min_stability, max_masked, max_errors):
         accumulative_matches = accumulative_sum(matches)
         accumulative_errors = accumulative_sum(errors)
 
+        # in order to search through masked pixels we search beyond
+        # min_stability + max_errors elements, this can cause problems if there
+        # are min_stability matches followed by max_errors + max_masked errors
+        # which should match as it has a sequence of min_stability matches, but
+        # will not because it has more than max_errors erros. To overcome this
+        # we only consider how many matches occur before max_errors errors
+        # occur.
         test = (accumulative_matches
                 .multiply(accumulative_errors.lte(max_errors))
                 .reduce(ee.Reducer.max())
                 .gte(min_stability))
 
+        # ensure that the first element matches to avoid off-by-one errors
         first_is_match = compare.select(0).eq(value)
         result = test.And(first_is_match)
 
@@ -46,6 +93,23 @@ def find_stable_sequence(events, value, min_stability, max_masked, max_errors):
 
 def find_stable_sequences(events, values, min_stabilities, max_masked,
                           max_errors):
+    """Find the first stable sequence of any of values in events.
+
+    Args:
+        events: An ee.Image whose bands represent classifications through time.
+        values: An ee.List of integers representing each of the class values of
+            interest to search for.
+        min_stabilities: An ee.List of integers representing the stability for
+            each of values.
+        max_masked: An ee.List of integers representing the maximum allowable
+            masked pixels in a stable sequence each of values.
+        max_errors: An ee.List of intergers representing the maximum allowable
+            errors in a stable sequence of each of values.
+
+    Returns:
+        An ee.Image with one band whose values represent the band index of the
+        input image where the first stable sequence of values occurs.
+    """
     indices = ee.List.sequence(0, values.length().subtract(1))
 
     def _match(index):
